ac9cacea76f0e2b359749c7bdb85279d23352a44
herikfernandes100 <herikfernandes100@gmail.com>
2025-05-16 16:09:53 -0300

    Botões básicos
    Botões numéricos e operadores básicos, concatenam no jtxVisor

--- Calculadora/src/calculadora/TelaCalculadora.java
+++ Calculadora/src/calculadora/TelaCalculadora.java
@@ -2,7 +2,6 @@ package calculadora;
 
 import java.awt.*;
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
 
 public class TelaCalculadora extends JFrame{
     
@@ -13,19 +12,26 @@ public class TelaCalculadora extends JFrame{
     private JTextField jtxVisor;
     private String[] simbolos = {"AC/C","+/-","%","RAIZ Q","/","7","8","9","x^Y","*","4","5","6","x^2","-","1","2","3","x^3","+","0",".","X!","10^X","="};
     private JButton[] jbBotoes;
+    private boolean digitandoNovoNumero = true;
+    private double valor1 = 0;
+    private double valor2 = 0;
+    private String operador = "";
    
     
     public TelaCalculadora(){
-        // Paineis
+
+        // ----- Paineis ----- 
         painel = new JPanel(); // Painel
         painelBotoes = new JPanel(); // Painel dos Botões
         painelVisor = new JPanel(); 
-        // Botões
+
+        // ----- Botões ----- 
         jbBotoes = new JButton[25];
         for(int i=0; i<25; i++){
             jbBotoes[i] = new JButton(simbolos[i]);
         }
-        // Visor
+
+        // ----- Visor ----- 
         jtxVisor = new JTextField();
     }
     
@@ -64,7 +70,61 @@ public class TelaCalculadora extends JFrame{
             jbBotoes[i].setFont(new Font("Arial", Font.PLAIN, 18)); // Define a fonte
         }
         painel.add(painelBotoes,BorderLayout.CENTER);
+
+        
+        // Ações dos botoes
+        jbBotoes[0].addActionListener(e->{ // AC/C
+
+        });
+        jbBotoes[1].addActionListener(e->{ // +/-
+
+        });
+        jbBotoes[2].addActionListener(e->{ // %
+
+        });
+        jbBotoes[3].addActionListener(e->{ // RAIZ Q
+
+        });
+        jbBotoes[4].addActionListener(e-> adicionarDigito("/"));
+        jbBotoes[5].addActionListener(e-> adicionarDigito("7"));
+        jbBotoes[6].addActionListener(e-> adicionarDigito("8"));
+        jbBotoes[7].addActionListener(e-> adicionarDigito("9"));
+        jbBotoes[8].addActionListener(e->{ // x^Y
+
+        });
+        jbBotoes[9].addActionListener(e-> adicionarDigito("*"));
+        jbBotoes[10].addActionListener(e-> adicionarDigito("4"));
+        jbBotoes[11].addActionListener(e-> adicionarDigito("5"));
+        jbBotoes[12].addActionListener(e-> adicionarDigito("6"));
+        jbBotoes[13].addActionListener(e->{ // x^2
+
+        });
+        jbBotoes[14].addActionListener(e-> adicionarDigito("-"));
+        jbBotoes[15].addActionListener(e-> adicionarDigito("1"));
+        jbBotoes[16].addActionListener(e-> adicionarDigito("2"));
+        jbBotoes[17].addActionListener(e-> adicionarDigito("3"));
+        jbBotoes[18].addActionListener(e->{ // x^3
+
+        });
+        jbBotoes[19].addActionListener(e-> adicionarDigito("+"));
+        jbBotoes[20].addActionListener(e-> adicionarDigito("0"));
+        jbBotoes[21].addActionListener(e-> adicionarDigito("."));
+        jbBotoes[22].addActionListener(e->{ // X!
+
+        });
+        jbBotoes[23].addActionListener(e->{ // 10^X
+
+        });
+        jbBotoes[24].addActionListener(e->{ // =
+
+        });
        
         
     }
+    
+    public void adicionarDigito(String digito){
+        jtxVisor.setText(jtxVisor.getText() + digito);
+    }
+
+
 }
